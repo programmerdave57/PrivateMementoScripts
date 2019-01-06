0b06eaa2e73d25e8c1e13cbc23496e32ebd640d6
David Lowell <dave750@outpost57.com>
2019-01-06 02:00:30 -0500

    bug fix

--- Movies.js
+++ Movies.js
@@ -3,7 +3,7 @@
 function setOmdbFields( o, e )
 {
     if ( o.Plot )
-        e.set( "plot", o.plot );
+        e.set( "plot", o.Plot );
     if ( o.Year )
         e.set( "year", o.Year );
     if ( o.Poster )
