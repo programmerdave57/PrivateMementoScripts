022ebf5b5c28fa971e3f7cb42c8abcfb1ff0d07c
David Lowell <dave750@outpost57.com>
2019-01-06 16:51:38 -0500

    first working version of setOmdbFields()

--- Movies.js
+++ Movies.js
@@ -1,11 +1,38 @@
 // Movies.js
 
-function setOmdbFields( o, e )
+function _setOmdbField( e, o, name )
 {
-    if ( o.Plot )
-        e.set( "plot", o.Plot );
-    if ( o.Year )
-        e.set( "year", o.Year );
-    if ( o.Poster )
-        e.set( "poster", o.Poster )
+    var mname;
+    var value;
+
+    mname = "o_" + name.toLowerCase();
+    e.set( mname, o[name] );
 }
+
+function setOmdbFields( e )
+{
+    var otext, o;
+    var fieldnames =
+        [ "Year", "Plot", "Rated", "Released",
+           "Runtime", "Genre", "Director", "Writer",
+           "Actors", "Poster", "Country", "Language",
+           "Production", "Website",
+           "Type", "totalSeasons",
+           "imdbID"
+        ];
+    var count, i;
+
+    otext = e.field( "OMDb Entry" );
+    if ( otext )
+        o = JSON.parse( otext );
+
+    if ( (o) && (o.Response == "True") )
+    {
+        count = fieldnames.length;
+        for ( i=0; i<count; i++ )
+            _setOmdbField( e, o, fieldnames[i] );
+    }
+}
+
+//e = entry();
+//setOmdbFields( e );
