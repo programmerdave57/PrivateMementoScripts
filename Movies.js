d54e1a8b344e2c0e79661e97c61abbb5c8b4c323
David Lowell <dave750@outpost57.com>
2019-01-08 01:11:19 -0500

    added importEntries()

--- Movies.js
+++ Movies.js
@@ -87,5 +87,79 @@ function getOmdbEntry( e )
     setOmdbFields( e );
 }
 
-//e = entry();
-//setOmdbFields( e );
+function importEntries()
+{
+    var lib, e;
+    var fname = "/storage/emulated/0/memento/movies_import.json";
+    
+    var data, text, f, one, title, obj;
+    var count, i, j;
+    var alist;
+    var source;
+    var found;
+    
+    f = file( fname );
+    text = f.readAll();
+    f.close();
+    data = JSON.parse( text );
+    
+    lib = lib();
+    count = data.titles.length;
+    for ( i=0; i<count; i++ )
+    {
+        one = data.titles[i];
+    
+        if ( one.Status != "new" )
+            continue;
+        if ( one.Action != "ADD" )
+            continue;
+    
+        title = one.Title;
+        e = lib.findByKey( title );
+        if ( ! e )
+        {
+            obj = {};
+            obj.Title = title;
+            obj.Availability = [one.Source];
+            obj.Imported = true;
+            e = lib.create( obj );
+    
+            getOmdbEntry( e );
+    
+            one.Status = "processed";
+            //break;
+        }
+        else
+        {
+            // entry exists...
+            source = one.Source;
+            alist = e.field("Availability");
+            found = false;
+            for ( j=0; j<alist.length; j++ )
+            {
+                if ( alist[j] == source )
+                {
+                    found = true;
+                     break;
+                 }
+            }
+            if ( ! found )
+            {
+                alist.push( source );
+                e.set( "Availability", alist );
+                one.Status = "processed";
+            }
+            else
+            {
+                one.Status = "duplicate";
+            }
+        }
+    }
+    
+    text = JSON.stringify( data );
+    f = file( fname );
+    f.write( text );
+    f.close();
+    
+    //message( data.titles.length );
+}
